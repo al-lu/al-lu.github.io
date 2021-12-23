f817b795a0e93ae5179db8a5f83c56a6e2f5ebff
al-lu <72130537+al-lu@users.noreply.github.com>
2021-12-23 15:48:19 +0200

    Wrote a binary converter and html to show ansver to the binary riddle

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -6,3 +6,19 @@
  * 
  * Add click events for work thumbnails to bring the user to my blog.
  */
+
+function binaryToString(str) {
+    var newBin = str.split(" ");
+    var binCode = [];
+    for (i = 0; i < newBin.length; i++) {
+        binCode.push(String.fromCharCode(parseInt(newBin[i], 2)));
+    }
+    return binCode.join("");
+
+}
+
+function giveAnswer(text) {
+    let binary = document.getElementById("binary").dataset.value;
+    let output = document.getElementById("output-text");
+    output.innerHTML = binaryToString(binary)
+}
