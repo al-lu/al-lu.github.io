af0ff4e9106c5047358482dc6d0ce588ea4bc5fc
al-lu <72130537+al-lu@users.noreply.github.com>
2021-12-23 18:35:16 +0200

    better commenting

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,10 +1,11 @@
 /**
  * Write binary number converter Here!
  * 
- * When yúser bring mouse over the element and clicks 
+ * When user bring mouse over the element and clicks, 
  * it triggers binary number conversion.
  * 
- * Add click events for work thumbnails to bring the user to my blog.
+ * Add click events for work thumbnails to bring the user to my blog. 
+ * 
  */
 
 function binaryToString(str) {
